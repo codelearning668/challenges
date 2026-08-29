5f6be266bc5a85aec98ef4fba0efc8b8b7b41be9
Martin Krajcovic <mkrajcovic@gratex.com>
2026-08-29 21:01:58 +0200

    [FIX] horsePower and torque are no longer required for car creation

--- src/main/java/sk/mkrajcovic/challenges/controller/dto/CreateCarRequest.java
+++ src/main/java/sk/mkrajcovic/challenges/controller/dto/CreateCarRequest.java
@@ -1,7 +1,6 @@
 package sk.mkrajcovic.challenges.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -17,10 +16,10 @@ public class CreateCarRequest {
 	@NotBlank @Size(max = 100)
 	private String name;
 
-	@NotNull @Positive
+	@Positive
 	private Integer hp;
 
-	@NotNull @Positive
+	@Positive
 	private Integer torque;
 
 	private WheelDrive drive; 
