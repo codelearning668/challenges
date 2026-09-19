37818f83bc11a715d4d0adf356caf517b958c79a
Martin Krajcovic <mkrajcovic@gratex.com>
2026-09-19 07:25:45 +0200

    [CODEREVIEW][SONAR] UserMapper - made private constructor (not designed to be instantiated) #S1118

--- src/main/java/sk/mkrajcovic/challenges/controller/mapper/UserMapper.java
+++ src/main/java/sk/mkrajcovic/challenges/controller/mapper/UserMapper.java
@@ -2,10 +2,13 @@ package sk.mkrajcovic.challenges.controller.mapper;
 
 import java.util.List;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import sk.mkrajcovic.challenges.controller.dto.UserInfoResponse;
 import sk.mkrajcovic.challenges.model.Authority;
 import sk.mkrajcovic.challenges.model.User;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserMapper {
 
 	public static UserInfoResponse toDetailResponse(User user) {
