9ab1669c9973f86773c1e98ff358998dd25a1e7d
mfathalipourux <mfathalipour@gratex.com>
2026-09-25 09:45:46 +0200

    [FIX] - fix typo

---
+++ src/main/java/sk/mkrajcovic/challenges/enums/SimulatorType.java
@@ -0,0 +1,9 @@
+package sk.mkrajcovic.challenges.enums;
+
+public enum SimulatorType {
+
+    ASSETTO_CORSA,
+    ASSETTO_CORSA_COMPETIZIONE,
+    WRC_GENERATIONS
+
+}
--- src/main/java/sk/mkrajcovic/challenges/model/SimulatorType.java
+++
@@ -1,9 +0,0 @@
-package sk.mkrajcovic.challenges.model;
-
-public enum SimulatorType {
-
-    ASSETO_CORSA,
-    ASSETO_CORSA_COMPETIZIONE,
-    WRC_GENERATIONS
-
-}
