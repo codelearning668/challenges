4fe8767d189227c204577b757f54aca4c77498ec
Martin Krajcovic <mkrajcovic@gratex.com>
2026-09-25 10:03:44 +0200

    [FIX] compilation errors on Simulator entity and SimulatorTypeConverter; added support for hyphen delimiters in URI for SimulatorType

--- src/main/java/sk/mkrajcovic/challenges/controller/converter/SimulatorTypeConverter.java
+++ src/main/java/sk/mkrajcovic/challenges/controller/converter/SimulatorTypeConverter.java
@@ -5,8 +5,8 @@ import java.util.Locale;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
+import sk.mkrajcovic.challenges.enums.SimulatorType;
 import sk.mkrajcovic.challenges.exception.ResourceNotFound;
-import sk.mkrajcovic.challenges.model.SimulatorType;
 
 /**
  * Converts simulator names provided as URI path variables to
@@ -21,7 +21,8 @@ public class SimulatorTypeConverter implements Converter<String, SimulatorType> 
 	@Override
 	public SimulatorType convert(String value) {
 		try {
-			return SimulatorType.valueOf(value.toUpperCase(Locale.ROOT));
+			var enumString = value.replace("-", "_").toUpperCase(Locale.ROOT);
+			return SimulatorType.valueOf(enumString);
 		} catch (IllegalArgumentException iaex) {
 			throw new ResourceNotFound("unknown simulator name");
 		}
--- src/main/java/sk/mkrajcovic/challenges/model/Simulator.java
+++ src/main/java/sk/mkrajcovic/challenges/model/Simulator.java
@@ -1,10 +1,11 @@
 package sk.mkrajcovic.challenges.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.Set;
+import sk.mkrajcovic.challenges.enums.SimulatorType;
 
 @Entity
 @Getter @Setter
